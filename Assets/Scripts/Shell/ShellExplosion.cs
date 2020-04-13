c5304eb2a50f058e9fd7af7c12fc74159f39a479
B. Bueno <afterglow9000@hotmail.com>
2020-04-13 02:48:17 -0600

    TANKS! Unity Tutorial - Phase 5 of 8 - Shells

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -8,24 +8,63 @@ public class ShellExplosion : MonoBehaviour
     public float m_MaxDamage = 100f;                  
     public float m_ExplosionForce = 1000f;            
     public float m_MaxLifeTime = 2f;                  
-    public float m_ExplosionRadius = 5f;              
+    public float m_ExplosionRadius = 5f;
 
+    /// <summary>
+    /// Calculate the amount of damage a target should take based on it's position.
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    /// <returns></returns>
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = explosionToTarget.magnitude;
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+        float damage = relativeDistance * m_MaxDamage;
+        damage = Mathf.Max(0f, damage);
+        return damage;
+    }
 
+    /// <summary>
+    /// Find all the tanks in an area around the shell and damage them.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        Collider[] colliders = Physics.OverlapSphere(
+            transform.position, m_ExplosionRadius, m_TankMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidbody)
+                continue;
+
+            targetRigidbody.AddExplosionForce(
+                m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+
+            if (!targetHealth)
+                continue;
+
+            float damage = CalculateDamage(targetRigidbody.position);
+            targetHealth.TakeDamage(damage);
+        }
+
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
     private void Start()
     {
         Destroy(gameObject, m_MaxLifeTime);
-    }
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        // Find all the tanks in an area around the shell and damage them.
-    }
-
-
-    private float CalculateDamage(Vector3 targetPosition)
-    {
-        // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
     }
 }
